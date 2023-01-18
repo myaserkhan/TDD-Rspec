ce54e215565a7901273bff235db0c363aa1b7f3a
Yasir Khan <khanyaser007@gmail.com>
2023-01-18 14:36:53 +0400

    Update factorial method to find factorial of num

--- solver.rb
+++ solver.rb
@@ -2,5 +2,7 @@ class Solver
   def factorial(number)
     raise ArgumentError, 'Only positive numbers are allowed' if number.negative?
     return 1 if number.zero?
+
+    factorial(number - 1) * number
   end
 end
