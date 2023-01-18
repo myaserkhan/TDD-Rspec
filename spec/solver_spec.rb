44646ca35a598b640149aa8d30706872d3b90fc6
dadadon <davianberoni@yahoo.com>
2023-01-18 09:55:01 -0500

    Add fizzbuzz tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,7 +13,7 @@ describe Solver do
 
   context 'When testing a factorial method' do
     it 'should raise an error when a negative integer is given' do
-      expect { @solver.factorial(-2) }.to raise_error ('Only positive numbers are allowed')
+      expect{ @solver.factorial(-2) }.to raise_error('Only positive numbers are allowed')
     end
 
     it 'should return 1 when given 0' do
@@ -28,6 +28,24 @@ describe Solver do
   context 'When testing a reverse method' do
     it 'should return olleh when given hello' do
       expect(@solver.reverse('hello')).to eql 'olleh'
+    end
+  end
+
+  context 'Fizz Buzz' do
+    it 'Should return Fizz when divisible by 3' do
+      expect(@solver.fizzbuzz(9)).to eql 'fizz'
+    end
+
+    it 'Should return Buzz when divisible by 5' do
+      expect(@solver.fizzbuzz(20)).to eql 'buzz'
+    end
+
+    it 'Should return FizzBuzz when divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+
+    it 'Should return Number as string when not divisible by 3 nor 5' do
+      expect(@solver.fizzbuzz(7)).to eql '7'
     end
   end
 end
